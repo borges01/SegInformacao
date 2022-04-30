50efda56bc31ec4b13b110cee13b329df96151fd
jborges42 <joao.borges@redacaonline.com.br>
2022-04-30 12:00:00 -0300

    created menu

--- main.c
+++ main.c
@@ -14,32 +14,48 @@
 // Função responsável por realizar o print do menu inicial
 void print_menu()
 {
-    printf("Menu \n");
-    printf("2 - Realizar Login \n");
-    printf("3 - Registrar-se \n");
+    printf("\n Menu \n");
+    printf("1 - Registrar-se \n");
+    printf("2 -Login \n");
     printf("0 - Sair \n");
 };
 
 // valida se a senha digitada está de acordo com a politica de senhas estabelecida
 // recebe a senha digitada
 // retorna 1 para senha de acordo com a politica e 0 para não
-int password_validate(char *password)
+int password_is_strong(char *password)
 {
-    return 0;
+    int password_size = strlen(password);
+
+    if (password_size != MAX_LENGTH_PASSWORD)
+    {
+        return 0;
+    }
+
+    return 1;
 }
 
+// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
+// Recebe a senha e o hash como paramentro
+// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
 char *password_hash(char *password)
 {
     char *hashed_password = crypt(password, "$6$rounds=20000$$");
     return hashed_password;
 }
 
-// Função responsável por retornar a senha do arquivo responsável por registrar os usuários
-// Recebe o ponteiro como parametro e retorna
-// Retorna o hash salvo no arquivo
-void password_of_file(FILE file)
+// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
+// Recebe a senha e o hash como paramentro
+// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
+int password_hash_compare(char *password, char *hashed_password)
 {
-    printf("sssssssssssssss");
+    // printf("\n\n Função password_hash_compare: \n %i\n", strcasecmp(hashed_password, password_hash(password)));
+    if (strcasecmp(hashed_password, password_hash(password)) == 0)
+    {
+        return 1;
+    }
+
+    return 0;
 };
 
 void register_user(char *first_name, char *last_name, char *password)
@@ -56,9 +72,7 @@ void register_user(char *first_name, char *last_name, char *password)
     printf("First name: %s \n\n", first_name);
     printf("%s\n\n", data_string);
 
-    FILE *fp;
-
-    fp = fopen(FILENAME, "a+");
+    FILE *fp = fopen(FILENAME, "a+");
 
     fputs(data_string, fp);
 
@@ -69,20 +83,17 @@ void register_user(char *first_name, char *last_name, char *password)
     printf("Usuário registrado com sucesso! \n\n");
 }
 
-void login()
+void login(char *input_first_name, char *input_password)
 {
-    FILE *fp;
 
-    fp = fopen(FILENAME, "r");
+    FILE *fp = fopen(FILENAME, "r");
 
-    char *first_name = malloc(MAX_LENGTH_FIRST_NAME);
+    char *first_name = malloc(1024);
     char *last_name = malloc(MAX_LENGTH_LAST_NAME);
-    char *hashed_password = malloc(MAX_LENGTH_DATA_STRING);
+    char *hashed_password = malloc(1024);
 
-    while (!feof(fp))
+    while (fscanf(fp, "%s;,;%s;,;%s;;\n", first_name, last_name, hashed_password) != EOF)
     {
-        fscanf(fp, "%s;,;%s;,;%s;;\n", first_name, last_name, hashed_password);
-
         printf("Nome: %s \nSobrenome %s \nHash %s\n\n", first_name, last_name, hashed_password);
     }
 
@@ -93,57 +104,58 @@ void login()
 
 int main(void)
 {
-    int option = 1;
+    int choice;
     char *first_name = malloc(MAX_LENGTH_FIRST_NAME);
     char *last_name = malloc(MAX_LENGTH_LAST_NAME);
     char *password = malloc(MAX_LENGTH_PASSWORD);
 
-    printf("Registre-se\n");
+    while (choice != 0)
+    {
+        print_menu();
+        scanf("%i", &choice);
 
-    // printf("Digite seu primeiro nome: \n");
-    // scanf("%s", first_name);
+        switch (choice)
+        {
+        case 1:
+            printf("\nRegistre-se\n");
 
-    // printf("Digite seu segundo nome: \n");
-    // scanf("%s", last_name);
+            printf("Digite seu primeiro nome: \n");
+            scanf("%s", first_name);
 
-    // printf("Digite sua senha: \n");
-    // scanf("%s", password);
+            printf("Digite seu segundo nome: \n");
+            scanf("%s", last_name);
 
-    login();
-    // register_user(first_name, last_name, password);
+            printf("Digite sua senha: \n");
+            printf("A senha deve conter 6 caracteres númericos: \n");
+            scanf("%s", password);
 
-    // while (option)
-    // {
-    //     print_menu();
-    //     scanf("%d\n", &option);
+            while (password_is_strong(password) != 1)
+            {
+                printf("A senha deve conter 6 caracteres númericos: \n");
+                scanf("%s\n", password);
+            }
 
-    //     switch (option)
-    //     {
-    //     case 2:
-    //         printf("Login");
-    //         break;
-    //     case 3:
+            register_user(first_name, last_name, password);
 
-    //         printf("Registre-se\n");
+            break;
+        case 2:
+            printf("Login\n");
 
-    //         printf("Digite seu primeiro nome: \n");
-    //         scanf("%s", first_name);
+            printf("Digite seu primeiro nome: \n");
+            scanf("%s", first_name);
 
-    //         printf("Digite seu segundo nome: \n");
-    //         scanf("%s", last_name);
+            printf("Digite sua senha: \n");
+            printf("A senha deve conter 6 caracteres númericos: \n");
+            scanf("%s", password);
 
-    //         printf("Digite sua senha: \n");
-    //         scanf("%s", password);
+            login(first_name, password);
 
-    //         register_user(first_name, last_name, password);
-    //         break;
-    //     case 0:
-    //         exit(0);
-
-    //     default:
-    //         printf("Digite uma opcao valida\n");
-    //     }
-    // }
+            break;
+        default:
+            printf("Digite uma opção válida!\n");
+            break;
+        }
+    }
 
     free(first_name);
     free(last_name);
