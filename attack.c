739d186498d4e7f3cc5564379e2cdb6951653b82
jborges42 <joao.borges@redacaonline.com.br>
2022-05-11 21:00:08 -0300

    created comments for teacher

--- attack.c
+++ attack.c
@@ -5,25 +5,28 @@
 #include <sys/random.h>
 #include <string.h>
 
-#define FILENAME "data.txt"
-#define ATTACKFILENAME "data-attack.txt"
+// constantes responsáveis por definir o tamanho máximo de cada campo
 #define MAX_LENGTH_FIRST_NAME 32
 #define MAX_LENGTH_LAST_NAME 32
-#define MAX_LENGTH_PASSWORD 6
+#define USERNAME_LENGTH 6
+#define PASSWORD_LENGTH 6
 #define MAX_LENGTH_DATA_STRING 512
+// constantes responsáveis por definir o nome padrão para os arquivos de dados e de ataque
+#define FILENAME "data.txt"
+#define ATTACKFILENAME "data-attack.txt"
 
-// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
+// Função responsável por retornar o hash de uma senha
 // Recebe a senha e o hash como paramentro
-// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
+// Retorna o poteiro de uma string com o hash gerado
 char *password_hash(char *password)
 {
     char *hashed_password = crypt(password, "$6$rounds=20000$$");
     return hashed_password;
 }
 
-// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
-// Recebe a senha e o hash como paramentro
-// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
+// Função responsável por comparar dois hash
+// Recebe o hash extraido do arquivo de dados e o hash de ataque
+// Retorna 1 caso os hash sejam equivalentes e 0 caso não sejam
 int password_hash_compare(char *database_hashed_password, char *attack_hashed_password)
 {
     if (strcasecmp(database_hashed_password, attack_hashed_password) == 0)
@@ -34,28 +37,20 @@ int password_hash_compare(char *database_hashed_password, char *attack_hashed_pa
     return 0;
 };
 
-void save_data(char *password, FILE *fp)
+// função responsável por comparar o hash extraido do arquivo de dados com os hashs do arquivo de ataque
+// percorrerá o arquivo de ataque por completo em busca de correspondencias
+// recebe como parametro o hash extraido do arquivo de dados
+// retorna 1 caso encontre correspondecia e 0 caso não encontre
+int find_hash_match(char *database_hashed_password)
 {
-    char *data_string = malloc(MAX_LENGTH_DATA_STRING);
-
-    strcat(data_string, password);
-    strcat(data_string, " ; ");
-    strcat(data_string, password_hash(password));
-    strcat(data_string, " ;;\n");
-
-    fputs(data_string, fp);
-
-    free(data_string);
-}
-
-int compare_hash(char *database_hashed_password)
-{
+    // váriavel que terá o retorno caso existe correspondencia ou não entre os hashs
     int password_hash_match = 0;
     FILE *fp = fopen(ATTACKFILENAME, "r");
 
-    char *file_password = malloc(MAX_LENGTH_PASSWORD);
+    char *file_password = malloc(PASSWORD_LENGTH);
     char *attack_hashed_password = malloc(MAX_LENGTH_DATA_STRING);
 
+    // percorre o arquivo todo e armazena as linhas nas respectivas variaveis
     while (fscanf(fp, "%s ; %s ;;\n", file_password, attack_hashed_password) != EOF)
     {
         if (password_hash_compare(database_hashed_password, attack_hashed_password) == 1)
@@ -65,6 +60,7 @@ int compare_hash(char *database_hashed_password)
         }
     }
 
+    // caso encontre correspondencias exibe no terminal qual é a senha e o hash correspondente
     if (password_hash_match == 1)
     {
         printf("\nSenha encontrada!\n");
@@ -77,14 +73,20 @@ int compare_hash(char *database_hashed_password)
 
     return password_hash_match;
 }
-
+// função responsável por percorrer o arquivo de dados e chamar a função que verifica se existe algum hash correspondente com o arquivo de ataque
 void find_password_hash()
 {
-
     FILE *fp = fopen(FILENAME, "r");
+
+    if (!fp)
+    {
+        printf("%s não encontrado no diretório. \n", FILENAME);
+        return;
+    }
 
     char *first_name = malloc(MAX_LENGTH_FIRST_NAME);
     char *last_name = malloc(MAX_LENGTH_LAST_NAME);
+    char *username = malloc(USERNAME_LENGTH);
     char *hashed_password = malloc(MAX_LENGTH_DATA_STRING);
 
     int password_hash_match = 0;
@@ -92,15 +94,19 @@ void find_password_hash()
     printf("Percorrendo arquivo de senhas em busca de correspondencias. \n");
     printf("Aguarde...\n");
 
-    while (fscanf(fp, "%s ; %s ; %s ;;\n", first_name, last_name, hashed_password) != EOF)
+    // percorre o arquivo de dados
+    while (fscanf(fp, "%s ; %s ; %s ; %s ;;\n", first_name, last_name, username, hashed_password) != EOF)
     {
-        if ((compare_hash(hashed_password)) == 1)
+        // chama a função responsável por comparar o hash do arquivo de dados com o hash do arquivo de ataque
+        //  se encontrar atribui 1 para a variavel password_hash_match e sai do loop
+        if ((find_hash_match(hashed_password)) == 1)
         {
             password_hash_match = 1;
             break;
         }
     }
 
+    // caso não encontre nenhuma correspondencia exibe uma mensagem dizendo que não foi encontrado
     if (password_hash_match == 0)
     {
         printf("\nNenhuma correspondencia encontrada!\n");
@@ -113,14 +119,35 @@ void find_password_hash()
     free(hashed_password);
 }
 
+// função responsável por salvar os dados no arquivo de ataque
+// recebe a senha de 6 digitos e o ponteiro de um arquivo
+// irá salvar no arquivo a senha e o seu respectivo hash, separados pelo seguinte padrão
+// senha ; hash ;;
+void save_data(char *password, FILE *fp)
+{
+    // ponteiro para a string que irá conter a linha que será salva no arquivo
+    char *data_string = malloc(MAX_LENGTH_DATA_STRING);
+
+    strcat(data_string, password);
+    strcat(data_string, " ; ");
+    strcat(data_string, password_hash(password));
+    strcat(data_string, " ;;\n");
+
+    // escreve a string no final do arquivo
+    fputs(data_string, fp);
+
+    free(data_string);
+}
+
+// função responsável por gerar todas as senhas de 6 digitos, do 0 ao 9
 void generate_all_numbers()
 {
     FILE *file = fopen(ATTACKFILENAME, "a+");
-    char *data_string = malloc(MAX_LENGTH_PASSWORD);
+    char *data_string = malloc(PASSWORD_LENGTH);
 
     printf("\nGerando arquivo com todas as senhas de 6 digitos do 0 ao 9.\n");
     printf("Aguarde...\n");
-
+    // laços de repetição para gerar a combinaçao das senhas
     for (int a = 0; a <= 9; a++)
     {
         for (int b = 0; b <= 9; b++)
@@ -133,8 +160,10 @@ void generate_all_numbers()
                     {
                         for (int f = 0; f <= 9; f++)
                         {
+                            // converte os números inteiros dos iteradores dos laços de repetição em uma string
                             sprintf(data_string, "%d%d%d%d%d%d", a, b, c, d, e, f);
 
+                            // passa a string gerada para a função responsável por salvar os dados no arquivo de ataque
                             save_data(data_string, file);
                         }
                     }
@@ -150,15 +179,18 @@ void generate_all_numbers()
 }
 int main(void)
 {
+    // verifica se o arquivo de ataque existe no diretório
     FILE *fp = fopen(ATTACKFILENAME, "r");
 
     if (fp)
     {
+        // se o arquivo existir, chama a função responsável por comparar o arquivo de dados com o arquivo de ataque
         find_password_hash();
     }
     else
     {
-        printf("Arquivo não encontrado. \n");
+        // caso o arquivo não exista, chama-se a função responsável por gerar todas as senhas e criar o arquivo contendo elas
+        printf("%s não encontrado. \n", ATTACKFILENAME);
         generate_all_numbers();
     }
 
--- main.c
+++ main.c
@@ -5,20 +5,38 @@
 #include <sys/random.h>
 #include <string.h>
 
+// constantes responsáveis por definir o tamanho máximo de cada campo
 #define MAX_LENGTH_FIRST_NAME 32
 #define MAX_LENGTH_LAST_NAME 32
-#define MAX_LENGTH_PASSWORD 6
+#define USERNAME_LENGTH 6
+#define PASSWORD_LENGTH 6
 #define MAX_LENGTH_DATA_STRING 512
+// constantes responsáveis por definir o nome padrão para o arquivo de dados
 #define FILENAME "data.txt"
 
 // Função responsável por realizar o print do menu inicial
 void print_menu()
 {
-    printf("Menu \n");
+    printf("\nMenu \n");
     printf("1 - Registrar-se \n");
     printf("2 - Login \n");
     printf("0 - Sair \n");
 };
+
+// valida se o usuário digitado possui 6 digitos
+// recebe a o usuário digitada
+// retorna 1 para usuário com a 6 digitos e 0 para não
+int username_length(char *username)
+{
+    int username_size = strlen(username);
+
+    if (username_size != USERNAME_LENGTH)
+    {
+        return 0;
+    }
+
+    return 1;
+}
 
 // valida se a senha digitada está de acordo com a politica de senhas estabelecida
 // recebe a senha digitada
@@ -27,7 +45,7 @@ int password_is_strong(char *password)
 {
     int password_size = strlen(password);
 
-    if (password_size != MAX_LENGTH_PASSWORD)
+    if (password_size != PASSWORD_LENGTH)
     {
         return 0;
     }
@@ -35,18 +53,18 @@ int password_is_strong(char *password)
     return 1;
 }
 
-// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
+// Função responsável por retornar o hash de uma senha
 // Recebe a senha e o hash como paramentro
-// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
+// Retorna o poteiro de uma string com o hash gerado
 char *password_hash(char *password)
 {
     char *hashed_password = crypt(password, "$6$rounds=20000$$");
     return hashed_password;
 }
 
-// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
+// Função responsável por verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
 // Recebe a senha e o hash como paramentro
-// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
+// Retorna 1 caso os hash sejam equivalentes e 0 caso não sejam
 int password_hash_compare(char *password, char *hashed_password)
 {
     if (strcasecmp(hashed_password, password_hash(password)) == 0)
@@ -57,12 +75,12 @@ int password_hash_compare(char *password, char *hashed_password)
     return 0;
 };
 
-// Função responsável por retornar verificar se o hash da senha digitada corresponde ao hash salvo no arquivo
-// Recebe a senha e o hash como paramentro
-// Retorna 1 caso os hash sejam equivalentes e 0 caso sejam falsos
-int first_name_compare(char *input_first_name, char *file_first_name)
+// Função responsável por verificar se o username digitado corresponde ao username salvo no arquivo
+// Recebe o username digitado no login e o username salvo no arquivo como paramentro
+// Retorna 1 caso os usernames sejam equivalentes e 0 caso não sejam
+int username_compare(char *input_username, char *file_username)
 {
-    if (strcasecmp(input_first_name, file_first_name) == 0)
+    if (strcasecmp(input_username, file_username) == 0)
     {
         return 1;
     }
@@ -70,47 +88,70 @@ int first_name_compare(char *input_first_name, char *file_first_name)
     return 0;
 };
 
-void register_user(char *first_name, char *last_name, char *password)
+// Função responsável por registrar um usuário em um arquivo
+//  recebe os dados do usuário como parametro
+void register_user(char *first_name, char *last_name, char *username, char *password)
 {
+    // ponteiro para a string que irá conter a linha que será salva no arquivo
     char *data_string = malloc(MAX_LENGTH_DATA_STRING);
 
+    // é criado a linha que será salva no arquivo, seguindo o seguinte padrão
+    // primeiro_nome ; sobrenome ; usuário ; hash_senha ;;
     strcat(data_string, first_name);
     strcat(data_string, " ; ");
     strcat(data_string, last_name);
+    strcat(data_string, " ; ");
+    strcat(data_string, username);
     strcat(data_string, " ; ");
     strcat(data_string, password_hash(password));
     strcat(data_string, " ;;\n");
 
     FILE *fp = fopen(FILENAME, "a+");
 
+    // a linha com os dados é inserida ao final do arquivo e com uma quebra de linha no final
     fputs(data_string, fp);
 
     fclose(fp);
 
     free(data_string);
 
-    printf("Usuário registrado com sucesso! \n");
+    printf("\nUsuário registrado com sucesso! \n");
 }
 
-void login(char *input_first_name, char *input_password)
+// função responsável por realizar o login do usuário
+// recebe o usuário e a senha como parametro
+void login(char *input_username, char *input_password)
 {
 
-    int authenticated = 0;
     FILE *fp = fopen(FILENAME, "r");
+
+    // caso não exista nenhum arquivo de dados a função login não é continuada
+    if (!fp)
+    {
+        printf("%s não encontrado no diretório. \n", FILENAME);
+        return;
+    }
+
+    int authenticated = 0;
 
     char *first_name = malloc(MAX_LENGTH_FIRST_NAME);
     char *last_name = malloc(MAX_LENGTH_LAST_NAME);
+    char *username = malloc(USERNAME_LENGTH);
     char *hashed_password = malloc(MAX_LENGTH_DATA_STRING);
 
-    while (fscanf(fp, "%s ; %s ; %s ;;\n", first_name, last_name, hashed_password) != EOF)
+    // percorre o arquivo de dados em busca do usuário digitado
+    while (fscanf(fp, "%s ; %s ; %s ; %s ;;\n", first_name, last_name, username, hashed_password) != EOF)
     {
-        if (password_hash_compare(input_password, hashed_password) == 1 && first_name_compare(input_first_name, first_name) == 1)
+        // verifica se a linha em questão possui usuário e senha correspondentes aos que foram digitados
+        if (username_compare(input_username, username) == 1 && password_hash_compare(input_password, hashed_password) == 1)
         {
+            // caso encontre correspondencia, a variável responsável por identificar o usuário como autenticado recebe 1 e o loop é interrompido
             authenticated = 1;
             break;
         }
     }
 
+    // caso o usuário esteja autenticado uma mensagem de boas vindas é exibido
     if (authenticated == 1)
     {
         printf("\nLogado!\n");
@@ -118,24 +159,31 @@ void login(char *input_first_name, char *input_password)
     }
     else
     {
+        // caso não seja encontrado nenhuma correspondencia no arquivo, uma mensagem é exibida para o usuário
         printf("\nErro ao tentar autenticar-se, verifique login e senha!\n");
     }
 
     fclose(fp);
     free(first_name);
     free(last_name);
+    free(username);
     free(hashed_password);
 }
 
 int main(void)
 {
+    // variável responsável por coletar a opção do usuário ao entrar no sistema
     int choice;
+    // variáveis responsáveis pelos dados digitados pelo usuário
     char *first_name = malloc(MAX_LENGTH_FIRST_NAME);
     char *last_name = malloc(MAX_LENGTH_LAST_NAME);
-    char *password = malloc(MAX_LENGTH_PASSWORD);
+    char *username = malloc(USERNAME_LENGTH);
+    char *password = malloc(PASSWORD_LENGTH);
 
+    // enquanto o usuário não digitar 0 para sair, o programa é executado
     while (choice != 0)
     {
+        // exibe o menu e recolhe a opção desejada pelo usuário
         print_menu();
         scanf("%i", &choice);
 
@@ -144,36 +192,52 @@ int main(void)
         case 1:
             printf("\nRegistre-se\n");
 
+            // caso o usuário digite 1, os dados de registro são solicitados
             printf("Digite seu primeiro nome: \n");
             scanf("%s", first_name);
 
             printf("Digite seu segundo nome: \n");
             scanf("%s", last_name);
 
+            printf("Digite seu usuário: \n");
+            printf("O usuário deve conter 6 caracteres alfanuméricos: \n");
+            scanf("%s", username);
+
+            // verifica o usuário digitado até ele ser um usuário válido de 6 digitos
+            while (username_length(username) != 1)
+            {
+                printf("O usuário deve conter 6 caracteres alfanuméricos: \n");
+                scanf("%s\n", username);
+            }
+
             printf("Digite sua senha: \n");
             printf("A senha deve conter 6 caracteres númericos: \n");
             scanf("%s", password);
 
+            // verifica a senha digitada até ela ser uma válido de 6 digitos
             while (password_is_strong(password) != 1)
             {
                 printf("A senha deve conter 6 caracteres númericos: \n");
                 scanf("%s\n", password);
             }
 
-            register_user(first_name, last_name, password);
+            // chama a função responsável por registrar os dados do usuário no arquivo de dados
+            register_user(first_name, last_name, username, password);
 
             break;
         case 2:
             printf("Login\n");
 
-            printf("Digite seu primeiro nome: \n");
-            scanf("%s", first_name);
+            // caso o usuário digite 2, os dados de login são solicitados
+            printf("Digite seu usuário: \n");
+            scanf("%s", username);
 
             printf("Digite sua senha: \n");
             printf("A senha deve conter 6 caracteres númericos: \n");
             scanf("%s", password);
 
-            login(first_name, password);
+            // chama a função responsável por realizar o login do usuário
+            login(username, password);
 
             break;
         case 0:
@@ -188,5 +252,6 @@ int main(void)
 
     free(first_name);
     free(last_name);
+    free(username);
     free(password);
 }
